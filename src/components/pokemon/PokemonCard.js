b210fceed78721a949f2a8befb334aa9cd556298
Macas <marczakmaciej2@gmail.com>
2020-04-05 21:51:33 +0200

    pokemon

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,12 +1,63 @@
 import React, { Component } from "react";
 
+import styled from "styled-components";
+
+const Sprite = styled.img`
+	width: 5em;
+	height: 5em;
+	display: none;
+`;
+
 export default class PokemonCard extends Component {
+	state = {
+		name: "",
+		imageUrl: "",
+		pokemonIndex: "",
+	};
+
+	componentDidMount() {
+		const { name, url } = this.props;
+		const pokemonIndex = url.split("/")[url.split("/").length - 2];
+		const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+
+		this.setState({
+			name,
+			imageUrl,
+			pokemonIndex,
+			imageLoading: true,
+			toManyRequests: false,
+		});
+	}
 	render() {
 		return (
 			<div className="col-md-3 col-sm-6 mb-5">
 				<div className="card">
-					<div className="card-header">
-						<h1>Maciej Pokemon</h1>
+					<h5 className="card-header">{this.state.pokemonIndex}</h5>
+					<Sprite
+						className="card-img-top rounded mx-auto mt-2"
+						src={this.state.imageUrl}
+						onLoad={() => this.setState({ imageLoading: false })}
+						onError={() => this.setState({ toManyRequests: true })}
+						style={
+							this.state.toManyRequests
+								? { display: "none" }
+								: this.state.imageLoading
+								? null
+								: { display: "block" }
+						}
+					/>
+					<div className="card-body mx-auto">
+						<h6 className="card-title ">
+							{this.state.name
+								.toLowerCase()
+								.split(" ")
+								.map(
+									letter =>
+										letter.charAt(0).toUpperCase() +
+										letter.substring(1)
+								)
+								.join("")}
+						</h6>
 					</div>
 				</div>
 			</div>
--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -1,25 +1,34 @@
 import React, { Component } from "react";
 import PokemonCard from "./PokemonCard";
+import axios from "axios";
 export default class PokemonList extends Component {
 	state = {
 		url: "https://pokeapi.co/api/v2/pokemon/",
 		pokemon: null,
 	};
 
-	componentDidMount() {}
+	async componentDidMount() {
+		const res = await axios.get(this.state.url);
+		this.setState({ pokemon: res.data["results"] });
+	}
 
 	render() {
 		return (
-			<div className="row">
-				<PokemonCard />
-				<PokemonCard />
-				<PokemonCard />
-				<PokemonCard />
-				<PokemonCard />
-				<PokemonCard />
-				<PokemonCard />
-				<PokemonCard />
-			</div>
+			<React.Fragment>
+				{this.state.pokemon ? (
+					<div className="row">
+						{this.state.pokemon.map(pokemon => (
+							<PokemonCard
+								key={pokemon.name}
+								name={pokemon.name}
+								url={pokemon.url}
+							/>
+						))}
+					</div>
+				) : (
+					<h1>Loading Pokemon</h1>
+				)}
+			</React.Fragment>
 		);
 	}
 }
