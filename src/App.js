a95bfe3c696a673613f58930ecd787aebf4eaf95
Macas <marczakmaciej2@gmail.com>
2020-04-05 20:15:42 +0200

    inital commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "../src/components/layout/NavBar";
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Navbar />
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class NavBar extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Maciej MArczak</h1>
+			</div>
+		);
+	}
+}
