525e0bb77a5a808f055c9b24b6fb3198e4b6b9c6
Macas <marczakmaciej2@gmail.com>
2020-04-12 00:02:30 +0200

    fix

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -4,8 +4,8 @@ export default class NavBar extends Component {
 	render() {
 		return (
 			<div>
-				<nav className=" navbar navbar-light bg-light fixed-top   ">
-					<a className="navbar brand col-sm-3 col-md-2 mr-0 align-items-center badge badge-primary text-wrap auto ">
+				<nav className=" navbar navbar-light bg-light fixed-top  ">
+					<a className="navbar brand col-sm-3 col-md-2 mr-0 align-items-center badge badge-primary text-center ">
 						Pokedex
 					</a>
 				</nav>
