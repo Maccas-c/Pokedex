190741a56e00b92eb213188ab720e542ef5821e6
Macas <marczakmaciej2@gmail.com>
2020-04-11 22:45:59 +0200

    add pagination

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -4,7 +4,7 @@ export default class NavBar extends Component {
 	render() {
 		return (
 			<div>
-				<nav className=" navbar navbar-light bg-light fixed-top  ">
+				<nav className=" navbar navbar-light bg-light fixed-top   ">
 					<a className="navbar brand col-sm-3 col-md-2 mr-0 align-items-center badge badge-primary text-wrap auto ">
 						Pokedex
 					</a>
--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -3,27 +3,68 @@ import PokemonCard from "./PokemonCard";
 import axios from "axios";
 export default class PokemonList extends Component {
 	state = {
-		url: "https://pokeapi.co/api/v2/pokemon?offset=&limit=100",
-		pokemon: null,
+		pokemonFetchedList: null,
+		pokemonListToDisplay: null,
+		page: 1,
 	};
+	url = "https://pokeapi.co/api/v2/pokemon?offset=&limit=120";
+	perPage = 12;
 
 	async componentDidMount() {
-		const res = await axios.get(this.state.url);
-		this.setState({ pokemon: res.data["results"] });
+		const res = await axios.get(this.url);
+		this.setState({
+			pokemonFetchedList: res.data["results"],
+			pokemonListToDisplay: res.data["results"].slice(0, this.perPage),
+		});
+	}
+
+	getPaginationArray() {
+		const start = 0;
+		const end = Math.ceil(
+			this.state.pokemonFetchedList.length / this.perPage
+		);
+		return Array(end - start)
+			.fill()
+			.map((_, idx) => start + idx + 1);
+	}
+
+	bindPageChange(idx) {
+		this.setState({
+			page: idx,
+		});
+		this.setState({
+			pokemonListToDisplay: this.state.pokemonFetchedList.slice(
+				this.state.page * this.perPage - this.perPage,
+				this.state.page * this.perPage
+			),
+		});
 	}
 
 	render() {
 		return (
 			<React.Fragment>
-				{this.state.pokemon ? (
-					<div className="row">
-						{this.state.pokemon.map(pokemon => (
-							<PokemonCard
-								key={pokemon.name}
-								name={pokemon.name}
-								url={pokemon.url}
-							/>
-						))}
+				{this.state.pokemonListToDisplay ? (
+					<div>
+						<div className="btn btn-outline-danger">
+							{this.getPaginationArray().map(num => (
+								<button
+									className="btn btn-light"
+									onClick={e => this.bindPageChange(num, e)}
+									key={num}
+								>
+									{num}
+								</button>
+							))}
+						</div>
+						<div className="row">
+							{this.state.pokemonListToDisplay.map(pokemon => (
+								<PokemonCard
+									key={pokemon.name}
+									name={pokemon.name}
+									url={pokemon.url}
+								/>
+							))}
+						</div>
 					</div>
 				) : (
 					<h1>Loading Pokemon</h1>
