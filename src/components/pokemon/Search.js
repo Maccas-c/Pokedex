eb99f69b3e8afc0034852328fab4302b672dc0b3
Macas <marczakmaciej2@gmail.com>
2020-04-15 20:44:14 +0200

    fix

--- src/components/pokemon/Search.js
+++ src/components/pokemon/Search.js
@@ -1,7 +1,36 @@
 import React, { Component } from "react";
 
-export default class Search extends Component {
+class Search extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {filteredPokemons = [],
+		};
+	}
+	searchFilter(input) {
+		let pokemons = this.state.pokemons;
+		let filteredPokemons = [];
+		pokemons.filter(pokemon => {
+			if (pokemon.name.includes(input.toLowerCase()) || input === "")
+				filteredPokemons.push(pokemon);
+			return null;
+		});
+		this.setState({
+			filteredPokemons,
+			
+		});
+	}
+
 	render() {
+		const filteredPokemons = this.state.filteredPokemons;
+        let pokemonsVisible = [];
+        if(!this.state.isLoading) {
+            for (let i = (((this.state.currentPage - 1) * this.state.pokemonsPerPage)); i < ((this.state.currentPage) * this.state.pokemonsPerPage); i++)
+                if(filteredPokemons[i]) pokemonsVisible.push(filteredPokemons[i]);
+        }
+
 		return <div></div>;
 	}
 }
+
+export default Search;
