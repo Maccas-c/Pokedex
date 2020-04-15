c6d8defc2cbcfd2dec3e95f93ce7e5ad341c90f9
Macas <marczakmaciej2@gmail.com>
2020-04-15 21:53:44 +0200

    fix

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -4,19 +4,41 @@ import axios from "axios";
 export default class PokemonList extends Component {
 	state = {
 		pokemonFetchedList: null,
-		
+		pokemonListToDisplay: null,
+		page: 1,
 	};
 	url = "https://pokeapi.co/api/v2/pokemon?offset=&limit=120";
-
+	perPage = 12;
 
 	async componentDidMount() {
 		const res = await axios.get(this.url);
 		this.setState({
 			pokemonFetchedList: res.data["results"],
-			
+			pokemonListToDisplay: res.data["results"].slice(0, this.perPage),
+		});
 	}
 
-	
+	getPaginationArray() {
+		const start = 0;
+		const end = Math.ceil(
+			this.state.pokemonFetchedList.length / this.perPage
+		);
+		return Array(end - start)
+			.fill()
+			.map((_, idx) => start + idx + 1);
+	}
+
+	bindPageChange(idx) {
+		this.setState({
+			page: idx,
+		});
+		this.setState({
+			pokemonListToDisplay: this.state.pokemonFetchedList.slice(
+				this.state.page * this.perPage - this.perPage,
+				this.state.page * this.perPage
+			),
+		});
+	}
 
 	render() {
 		return (
@@ -34,7 +56,15 @@ export default class PokemonList extends Component {
 								</button>
 							))}
 						</div>
-						
+						<div className="row">
+							{this.state.pokemonListToDisplay.map(pokemon => (
+								<PokemonCard
+									key={pokemon.name}
+									name={pokemon.name}
+									url={pokemon.url}
+								/>
+							))}
+						</div>
 					</div>
 				) : (
 					<h1>Loading Pokemon</h1>
